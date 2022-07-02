280eb5f7696b44cfbec611e118a9c6d75f838361
IvannaGumeniuk <vany93ok@gmail.com>
2022-07-02 23:02:57 +0300

    done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,7 @@
-export const fetchCountries = name => {
-    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,name.official,capital,population,flags,languages`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            
-            return response.json();
-        });
-};
+export function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,name.official,capital,population,languages,flags`,
+  ).then(response => {
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,44 +1,86 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
 
+var debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  inputCountry: document.querySelector('#search-box'),
+  countryContainer: document.querySelector(".country-info"),
+  countryList: document.querySelector(".country-list"),
+}
 
-const inputCountry = document.querySelector('#search-box');
-const countruContainer = document.querySelector(".country-info");
-inputCountry.addEventListener('input', event => {
-    event.preventDefault();
+refs.inputCountry.addEventListener('input', debounce(OnInput, DEBOUNCE_DELAY));
 
-   fetchCountries(event.target.value).then(country => {
-        countruContainer.innerHTML = countryCardsMarkup(country);
-        console.log(countryCardsMarkup(country));
-        // countryCardsMarkup(country);
+function OnInput(event) {
+  const inputValue = event.target.value.trim();
+  if (!inputValue) {
+    clearInterface();
+    return;
+  }
+
+  fetchCountries(inputValue)
+    .then(countries => {
+      if (countries.length > 10) {
+        clearInterface();
+        return Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+      } else if (countries.length < 10 && countries.length > 1) {
+        return renderListCountry(countries);
+      } else if (countries.length === 1){
+        countryCardsMarkup(countries);
+      }
+    })
+    .catch(error => {
+      return Notiflix.Notify.failure('Oops, there is no country with that name');
+      clearInterface();
     });
-    
-});
+  
+  if (inputValue === "russia") {
+    return Notiflix.Notify.warning('russian warship, fuck you!!!');
+  }
+}
 
+function renderListCountry(countries) {
+  const countryInfo = countries
+    .map(({ name, flags }) => {
+      return `
+      <li class="country-list__item">
+        <img src="${flags.svg}" alt="${name.official}" width="25px"> <span>${name.official}</span>
+      </li>
+      `;
+    })
+    .join('');
+  refs.countryContainer.innerHTML = '';
+  refs.countryList.innerHTML = countryInfo;
+}
 
-function countryCardsMarkup({name, flags, capital, population, languages }) {
-    return `
-            <div class="country-cards-tamplate">
+function countryCardsMarkup(countries) {
+  const markup = countries
+    .map(({ name, capital, population, languages, flags }) => {
+      const languagesList = Object.values(languages);
+      return `
+<div class="country-cards-tamplate">
+
 <div class="card-img-top">
-  <img src="${flags}" alt="${name}">
+  <img src="${flags.svg}" class="country-img" alt="${name.official}" width="25px" 
+  <span class="card-title"><b>${ name.official.toUpperCase() }</b></span>
 </div>
 
 <div class="card-body">
-  <h2 class="card-title">Повна назва країни: ${name}</h2>
-  <p class="card-text">Столиця: ${capital}</p>
-  <p class="card-text">Населення: ${population}</p>
-  
-  <p class="card-text">Масив мов:</p>
-  
-  <ul>
-  {{#each languages}}
-<li class="languages-list">${{languages}}</li>
-    {{#each}}
-  </ul>
+
+  <p class="card-text"><b>Столиця:</b> ${capital}</p>
+  <p class="card-text"><b>Населення:</b> ${population}</p>
+  <p class="card-text"><b>Масив мов:</b> ${languagesList}</p>
 
 </div>
 </div>
     `
-};
-    
+    }).join('');
+  refs.countryList.innerHTML = '';
+  refs.countryContainer.innerHTML = markup;
+}
+
+function clearInterface() {
+    refs.countryList.innerHTML = '';
+    refs.countryContainer.innerHTML = '';
+}
